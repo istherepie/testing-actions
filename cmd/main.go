f37deb2174e8ca367806e075b35576a0163fdf7f
Steffen Park <dev@istherepie.com>
2021-05-15 23:21:18 +0200

    Add testapp

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/istherepie/testing-actions/internal/service"
+)
+
+func App(instance string, host string, port int) int {
+
+	addr := fmt.Sprintf("%v:%d", host, port)
+
+	listener, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Println(err)
+		return 1
+	}
+
+	mux := service.New(instance)
+
+	log.Printf("Server starting on %v", addr)
+	serveErr := http.Serve(listener, mux)
+
+	if serveErr != nil {
+		log.Println(serveErr)
+		return 1
+	}
+
+	return 0
+}
 
 func main() {
-	fmt.Println("Hello World")
+	instance := flag.String("instance", "unknown", "Instance name.")
+	host := flag.String("host", "localhost", "Service host.")
+	port := flag.Int("port", 8080, "Service port.")
+	flag.Parse()
+
+	retCode := App(*instance, *host, *port)
+	os.Exit(retCode)
 }
